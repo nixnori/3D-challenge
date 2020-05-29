f991be3e0c9af7f7f333f73650177c60ae95333b
Nikki <aletheanicole.n@gmail.com>
2020-05-29 15:34:18 -0400

    Finish header. Main ready for cards

--- 3d-challenge/src/components/Header/Header.js
+++ 3d-challenge/src/components/Header/Header.js
@@ -2,14 +2,17 @@ import React, { Component } from 'react';
 import './Header.scss';
 import hamburger from '../../assets/icon/hamburger.svg';
 import logo from '../../assets/logo/logo.png';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
     render() {
         return (
             <div className="header">
-                <img src={hamburger} />
-                <img src={logo} />
-                
+                <div className="header__container">
+                    <img className="header__menu" src={hamburger} />
+                    <img className="header__logo" src={logo} />
+                </div>
+                <Link to="" className="header__link">Updated COVID-19 Business Regulations</Link>
             </div>
         )
     }
--- 3d-challenge/src/pages/Main/Main.js
+++ 3d-challenge/src/pages/Main/Main.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import './Main.scss';
 
 class Main extends Component {
     
@@ -8,6 +8,10 @@ class Main extends Component {
             <div className="main">
                 <h1 className="main__header">Hello, Ms. Sharon</h1>
                 <h4 className="main__business-name">Mamma Mia Restaurant</h4>
+                <div className="main__overview">
+                    <h1 className="overview__header">Overview</h1>
+                    <h3 className="overview__title">Owner</h3>
+                </div>
             </div>
         )
     }
