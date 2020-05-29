3e790d5f9bd1f534426dea4896f026bb78915152
Nikki <aletheanicole.n@gmail.com>
2020-05-29 14:11:48 -0400

    Change location of Header

--- 3d-challenge/src/App.js
+++ 3d-challenge/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Switch>
         <Route path="/" exact component={Main} />
       </Switch>
